095c6fa778ef4bc431a4cea278d1f6782c2bb3b4
Dovile Tumaite <dovile.tumaite@yahoo.it>
2022-02-28 16:06:33 +0100

    sidebar starting point

---
+++ components/Sidebar.tsx
@@ -0,0 +1,12 @@
+import HomeIcon from "@heroicons/react/outline/HomeIcon"
+
+const Sidebar = () => {
+    return(
+        <div>
+            <button>
+                <HomeIcon className="h-5 w-5"/>
+            </button>
+        </div>
+    )
+}
+export default Sidebar
